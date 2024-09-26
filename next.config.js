d0f7d34ab492b1378f066124fd8c0efc5e8fbffa
Johnny <53662042+Green-Thanos@users.noreply.github.com>
2024-09-26 19:36:16 -0400

    Content Update

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  async redirects() {
+    return [
+      {
+        source: '/legacy',
+        destination: 'https://legacy.helivox.com',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
